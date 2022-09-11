00892266975fb575833de32e2df3f0492853cd40
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-11 19:09:37 -0300

    Added search by name option

--- src/components/Repositories/Repositories.jsx
+++ src/components/Repositories/Repositories.jsx
@@ -4,32 +4,41 @@ import { gitHubApi } from "../Api/gitHubApi";
 
 export function Repositories() {
   const [repositories, setRepositories] = useState([]);
+  const [repoName, setRepoName] = useState("");
 
   async function selectName(event) {
     event.preventDefault();
-
-    const repos = await gitHubApi.getRepositories(event.target.name.value)
-    setRepositories([...repos])
+    const repos = await gitHubApi.getRepositories(event.target.name.value);
+    setRepositories([...repos]);
   }
 
-
+  function selectRepo(event) {
+    event.preventDefault();
+    setRepoName(event.target.name.value);
+  }
 
   return (
     <div className="Repositories">
       <form onSubmit={selectName}>
-        <input
-          type="text"
-          name="name"
-        ></input>
+        <input type="text" name="name" placeholder="Github username"></input>
         <button type="submit" className="btn-submit">
-          Submit
+          Search
+        </button>
+      </form>
+
+      <form onSubmit={selectRepo}>
+        <input type="text" name="name" placeholder="Repository name"></input>
+        <button type="submit" className="btn-submit">
+          Search
         </button>
       </form>
 
       {repositories === [] ? (
         <p>Loading...</p>
       ) : (
-        repositories.map((repo) => <Repository repo={repo} />)
+        repositories.map((repo) => (
+          <Repository repo={repo} repoName={repoName} />
+        ))
       )}
     </div>
   );
--- src/components/Repository/Repository.jsx
+++ src/components/Repository/Repository.jsx
@@ -1,10 +1,12 @@
 export default function Repository(props) {
-  return (
-    <section>
-      <div>{props.repo.name}</div>{" "}
-      <a href={props.repo.svn_url} target="_blank">
-        <button>Ver repositório</button>
-      </a>
-    </section>
-  );
+  if (props.repo.name.toLowerCase().includes(props.repoName.toLowerCase())) {
+    return (
+      <section>
+        <div>{props.repo.name}</div>{" "}
+        <a href={props.repo.svn_url} target="_blank">
+          <button>Ver repositório</button>
+        </a>
+      </section>
+    );
+  }
 }
