7ad294e873fff212a94cf6caf94165b2d68558a5
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-11 10:40:26 -0300

    Imported repositories

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Repository from "../Repository/Repository";
+import { Repositories } from "../Repositories/Repositories";
 
 export default function Body() {
-  const [repositories, setRepositories] = useState([]);
-  useEffect(() => {
-    fetch(`https://api.github.com/users/DouglasVolcato/repos`)
-      .then((response) => response.json())
-      .then((data) => {
-        setRepositories([...data]);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
-
   return (
     <div className="Body">
-      {repositories === [] ? (
-        <p>Loading...</p>
-      ) : (
-        repositories.map((repo) => <Repository repo={repo} />)
-      )}
+      <Repositories />
     </div>
   );
 }
