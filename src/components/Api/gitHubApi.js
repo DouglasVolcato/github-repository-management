5464d96838b0639ac020f8b7862d739ed89af91a
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-11 19:09:18 -0300

    Increased array length for fetch api

--- src/components/Api/gitHubApi.js
+++ src/components/Api/gitHubApi.js
@@ -2,7 +2,7 @@ const baseUrl = "https://api.github.com/users/";
 
 export const gitHubApi = {
   getRepositories: async (userName) => {
-    const response = await fetch(baseUrl + userName + "/repos", {
+    const response = await fetch(baseUrl + userName + "/repos?per_page=10000", {
       method: "GET",
     });
     const data = await response.json();
