1d1dfc414821c6790e63ec4299cc4ae722aa4555
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 12:04:50 -0300

    Added Register import

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,10 +1,12 @@
 import Repositories from "../Repositories/Repositories";
 import Notes from "../Notes/Notes";
 import Login from "../Login/Login";
+import Register from "../Register/Register";
 
 export default function Body() {
   return (
     <div className="Body">
+      <Register />
       <Login />
       <Notes />
       <Repositories />
