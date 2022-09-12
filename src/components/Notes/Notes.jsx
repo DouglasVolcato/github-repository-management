0a1a79b987fb18e9741f44d5914435c952ba9daa
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 00:57:25 -0300

    Added Notes

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,2 +1,22 @@
-// Show the hole list of repositories with notes by userId
-// Option to create and update notes
+import React, { useState } from "react";
+import Note from "../Note/Note";
+import { repositoryManagementApi } from "../Api/repositoryManagementApi";
+
+export default function Notes() {
+  const [notes, setNotes] = useState([]);
+
+  async function getNotes() {
+    const data = await repositoryManagementApi.getAllRepo();
+    setNotes([...data]);
+  }
+
+  getNotes();
+
+  return (
+    <div className="Notes" onChange={getNotes}>
+      {notes.map((data) => (
+        <Note data={data} />
+      ))}
+    </div>
+  );
+}
