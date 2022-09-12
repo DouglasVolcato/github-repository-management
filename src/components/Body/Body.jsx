5546048bd0008c4675629f48ae79b1400bdb3cf3
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 17:12:51 -0300

    Added pagination

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -2,21 +2,20 @@ import Repositories from "../Repositories/Repositories";
 import Notes from "../Notes/Notes";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
-import { useState } from "react";
+import "./Body.css";
 
-export default function Body() {
-  const [newToken, setNewToken] = useState(0);
-
-  function newLogin() {
-    setNewToken(newToken + 1);
+export default function Body(props) {
+  function showPage() {
+    if (props.page === "login") {
+      return <Login />;
+    } else if (props.page === "register") {
+      return <Register />;
+    } else if (props.page === "notes") {
+      return <Notes />;
+    } else if (props.page === "repositories") {
+      return <Repositories />;
+    }
   }
 
-  return (
-    <div className="Body">
-      <Register />
-      <Login newLogin={newLogin} />
-      <Notes />
-      <Repositories />
-    </div>
-  );
+  return <div className="Body">{showPage()}</div>;
 }
