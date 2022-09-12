c0aa003ca78bcbe5756b0f4389c87eba02fc6b6c
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 11:52:44 -0300

    Added  Login in imports

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,11 @@
 import Repositories from "../Repositories/Repositories";
 import Notes from "../Notes/Notes";
+import Login from "../Login/Login";
 
 export default function Body() {
   return (
     <div className="Body">
+      <Login />
       <Notes />
       <Repositories />
     </div>
