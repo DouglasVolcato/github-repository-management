bd434f1b953bda76217ed13f156bfb792ff2f22a
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 11:52:25 -0300

    Added Login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { repositoryManagementApi } from "../Api/repositoryManagementApi";
+
+export default function Login() {
+  const [loginUser, setLoginUser] = useState();
+
+  async function logUser(event) {
+    event.preventDefault();
+
+    setLoginUser({ ...loginUser });
+
+    repositoryManagementApi.login(loginUser);
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={logUser}>
+        Email:{" "}
+        <input
+          type="text"
+          name="email"
+          onChange={(event) => {
+            setLoginUser({ ...loginUser, email: event.target.value });
+          }}
+        />{" "}
+        <br />
+        Password:{" "}
+        <input
+          type="text"
+          name="password"
+          onChange={(event) => {
+            setLoginUser({ ...loginUser, password: event.target.value });
+          }}
+        />{" "}
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
