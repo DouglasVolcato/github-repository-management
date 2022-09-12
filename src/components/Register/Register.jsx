42ba0ae88d87e0723e5b73c40f43823d79992704
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 12:04:37 -0300

    Added user registration

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { repositoryManagementApi } from "../Api/repositoryManagementApi";
+
+export default function Register() {
+  const [registerUser, setRegisterUser] = useState();
+
+  async function addNewUser(event) {
+    event.preventDefault();
+
+    setRegisterUser({ ...registerUser });
+
+    repositoryManagementApi.createUser(registerUser);
+  }
+
+  return (
+    <div className="Register">
+      <form onSubmit={addNewUser}>
+        Name:
+        <input
+          type="text"
+          name="name"
+          onChange={(event) => {
+            setRegisterUser({ ...registerUser, name: event.target.value });
+          }}
+        />
+        <br />
+        Email:
+        <input
+          type="text"
+          name="email"
+          onChange={(event) => {
+            setRegisterUser({ ...registerUser, email: event.target.value });
+          }}
+        />
+        <br />
+        Password:
+        <input
+          type="text"
+          name="password"
+          onChange={(event) => {
+            setRegisterUser({ ...registerUser, password: event.target.value });
+          }}
+        />
+        <br />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
