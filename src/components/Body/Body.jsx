757876d5be8521c68850b268548d34a13a432f0f
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 00:58:40 -0300

    Imported Notes

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,8 +1,10 @@
-import { Repositories } from "../Repositories/Repositories";
+import Repositories from "../Repositories/Repositories";
+import Notes from "../Notes/Notes";
 
 export default function Body() {
   return (
     <div className="Body">
+      <Notes />
       <Repositories />
     </div>
   );
