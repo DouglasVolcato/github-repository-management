dcee0c03fb25e2c40f63cde744f99dfb1e015cd5
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 17:12:04 -0300

    Added footer and header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,16 @@
 import Body from "../Body/Body";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { useState } from "react";
 
 export default function App() {
+  const [page, setPage] = useState("login");
+
   return (
     <div className="App">
-      <Body />
+      <Header setPage={setPage}/>
+      <Body page={page}/>
+      <Footer />
     </div>
   );
 }
