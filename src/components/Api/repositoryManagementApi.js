e53b2ee2b40d8c8b4d417cc2d961b0e430f00444
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 00:59:22 -0300

    Added repositoryManagement API

---
+++ src/components/Api/repositoryManagementApi.js
@@ -0,0 +1,59 @@
+const baseUrl = "http://localhost:3001";
+
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImM2NTJiNDgzLTA0YWItNGFkZS04YzYwLWQzM2U5NjZjMjBhNiIsImlhdCI6MTY2MjkzOTY0OCwiZXhwIjoxNjYzMDI2MDQ4fQ.VQHi1cSPuulY76WggH2Slc3MaJq9cLE8VMH7nRCg03s";
+
+export const repositoryManagementApi = {
+  getAllRepo: async () => {
+    const response = await fetch(baseUrl + "/repo/get-all-repository", {
+      method: "GET",
+      headers: new Headers({
+        Authorization: "Bearer " + token,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }),
+    });
+    const data = await response.json();
+
+    return data;
+  },
+
+  deleteRepo: async (repoName) => {
+    const response = await fetch(
+      baseUrl + "/repo/delete-repository/" + repoName,
+      {
+        method: "DELETE",
+        headers: new Headers({
+          Authorization: "Bearer " + token,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        }),
+      }
+    );
+    const data = await response.json();
+
+    return data;
+  },
+
+  editRepo: async (repoName, repoBody) => {
+    console.log(JSON.stringify(repoBody));
+
+    const response = await fetch(
+      baseUrl + "/repo/update-repository/" + repoName,
+      {
+        method: "PUT",
+        headers: new Headers({
+          Authorization: "Bearer " + token,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        }),
+        body: JSON.stringify({ ...repoBody }),
+      }
+    );
+    const data = await response.json();
+
+    console.log(data);
+
+    return data;
+  },
+};
