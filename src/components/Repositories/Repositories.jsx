b71a2d758a712f6feac156a1a3e9ba70bffefe8c
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 00:56:29 -0300

    Added default export

--- src/components/Repositories/Repositories.jsx
+++ src/components/Repositories/Repositories.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Repository from "../Repository/Repository";
 import { gitHubApi } from "../Api/gitHubApi";
 
-export function Repositories() {
+export default function Repositories() {
   const [repositories, setRepositories] = useState([]);
   const [repoName, setRepoName] = useState("");
 
