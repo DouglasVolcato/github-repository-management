6e51a53b47730b97026eb06737a6a47012204dfa
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 01:12:29 -0300

    Added createNote function

--- src/components/Repository/Repository.jsx
+++ src/components/Repository/Repository.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+import { repositoryManagementApi } from "../Api/repositoryManagementApi";
+
 export default function Repository(props) {
+  async function createNote() {
+    return await repositoryManagementApi.addRepo({
+      name: props.repo.name,
+      link: props.repo.svn_url,
+      priority: "Low",
+      note: "Empty",
+    });
+  }
+
   if (props.repo.name.toLowerCase().includes(props.repoName.toLowerCase())) {
     return (
       <section>
@@ -6,6 +18,7 @@ export default function Repository(props) {
         <a href={props.repo.svn_url} target="_blank" rel="noreferrer">
           <button>Access</button>
         </a>
+        <button onClick={() => createNote()}>Add note</button>
       </section>
     );
   }
