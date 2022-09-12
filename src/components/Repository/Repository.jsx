fe3feabf0a282fddedf70b0bc97890cf2b344117
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-12 00:56:10 -0300

    Changed access button

--- src/components/Repository/Repository.jsx
+++ src/components/Repository/Repository.jsx
@@ -3,8 +3,8 @@ export default function Repository(props) {
     return (
       <section>
         <div>{props.repo.name}</div>{" "}
-        <a href={props.repo.svn_url} target="_blank">
-          <button>Ver repositório</button>
+        <a href={props.repo.svn_url} target="_blank" rel="noreferrer">
+          <button>Access</button>
         </a>
       </section>
     );
