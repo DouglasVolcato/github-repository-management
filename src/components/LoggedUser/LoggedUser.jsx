a260dbb69b17419024370efc2e289c8407b80c8a
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-18 02:31:34 -0300

    Added user keys

--- src/components/LoggedUser/LoggedUser.jsx
+++ src/components/LoggedUser/LoggedUser.jsx
@@ -1,34 +1,49 @@
 import { repositoryManagementApi } from "../Api/repositoryManagementApi";
 import { useState, useEffect } from "react";
+import ExtraSecurity from "../extraSecurity/ExtraSecurity";
 import "./LoggedUser.css";
 
 export default function LoggedUser(props) {
   const [user, setUser] = useState({});
+  const [keyReferences, setkeyReferences] = useState([]);
+
+  const loginInfo = props.loginInfo;
 
   async function getLoggedUser() {
     const response = await repositoryManagementApi.getUserById();
     setUser({ ...response });
     props.setFullUserProfile({ ...response });
+    getUserKeyReferences(response.email);
+  }
+
+  async function getUserKeyReferences(UserEmail) {
+    const data = await repositoryManagementApi.getSecurityKeys(UserEmail);
+    setkeyReferences([...data]);
   }
 
   useEffect(() => {
     getLoggedUser();
-  }, [props.loginInfo]);
+  }, [loginInfo]);
 
   return (
     <div className="LoggedUser">
       {user.name ? (
-        <button
-          type="button"
-          className="LoggedUser__logged"
-          onClick={() => props.setShowUserModal(true)}
-        >
-          ✓ Logged as {user.name}
-        </button>
+        <div>
+          <button
+            type="button"
+            className="LoggedUser__logged"
+            onClick={() => props.setShowUserModal(true)}
+          >
+            ✓ Logged as {user.name}
+          </button>
+
+          <ExtraSecurity user={user} keyReferences={keyReferences} />
+        </div>
       ) : (
         <div>
-          <h2 className="LoggedUser__notLogged">Make login to access</h2>
-          <h2 className="LoggedUser__notLogged">the notes.</h2>
+          <h2 className="LoggedUser__notLogged">
+            Make login to access the notes.
+          </h2>
         </div>
       )}
     </div>
