e751e2b2c6446dbbf4c79f00ecf5c251fbfbc590
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-20 01:39:31 -0300

    Added notes sorted by deadline

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -8,17 +8,29 @@ export default function Notes() {
   const [notes, setNotes] = useState([]);
 
   async function getNotes() {
-    const data = await repositoryManagementApi.getAllRepo().then((notes) =>
-      notes.sort((a, b) => {
-        return a.priority === "High" && b.priority !== "High"
-          ? -1
-          : a.priority === "Medium" &&
-            b.priority !== "Medium" &&
-            b.priority !== "High"
-          ? -1
-          : 1;
-      })
-    );
+    const data = await repositoryManagementApi
+      .getAllRepo()
+      .then((notes) =>
+        notes.sort((a, b) => {
+          return a.priority === "High" && b.priority !== "High"
+            ? -1
+            : a.priority === "Medium" &&
+              b.priority !== "Medium" &&
+              b.priority !== "High"
+            ? -1
+            : 1;
+        })
+      )
+      .then((notes) =>
+        notes.sort((a, b) => {
+          return a.deadline > b.deadline
+            ? -1
+            : a.deadline === b.deadline
+            ? 0
+            : 1;
+        })
+      );
+
     setNotes([...data]);
   }
 
